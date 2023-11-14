34477b421c6a3553d4fd6cc2570a93fa6da60f11
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-11-13 21:00:34 -0500

    part2

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -34,7 +34,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void save(UserDTO dto) {
-
+        userRepository.save(userMapper.convertToEntity(dto));
     }
 
     @Override

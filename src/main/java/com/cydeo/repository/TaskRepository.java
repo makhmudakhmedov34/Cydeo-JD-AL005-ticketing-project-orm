fcfee5bbb7cdb41716a5cbf6378afbe36e9f14d9
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-11-20 20:15:30 -0500

    part4

--- src/main/java/com/cydeo/repository/TaskRepository.java
+++ src/main/java/com/cydeo/repository/TaskRepository.java
@@ -11,9 +11,9 @@ import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task,Long> {
 
-    @Query("SELECT COUNT (t) from Task t where t.project.projectCode=?1 and t.project.projectStatus<>'Completed'")
+    @Query("SELECT COUNT (t) from Task t where t.project.projectCode=?1 and t.project.projectStatus<>'COMPLETE'")
     int totalNonCompletedTasks(String projectCode);
 
-    @Query(value = "SELECT count(*) FROM tasks t join projects p on t.project_id = p.project_id where p.project_code =?1 and t.task_status = 'COMPLETED'",nativeQuery = true)
+    @Query(value = "SELECT count(*) FROM tasks t join projects p on t.project_id = p.id where p.project_code =?1 and t.task_status = 'COMPLETE'",nativeQuery = true)
     int totalCompletedTasks(String projectCode);
 }
